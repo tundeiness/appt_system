568fd024dd09c93f822e77526902d1519687470c
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-07-30 18:13:21 -0400

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :role, presence: true
+
+  has_one :client
+  has_one :therapist
+  has_one :admin
+
+  enum role: { client: 0, therapist: 1, admin: 2 }
 end

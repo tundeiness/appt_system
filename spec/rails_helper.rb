8da53391d981ba6cdc36fbeddb3f5c0e763c1cc4
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-07-30 23:06:06 -0400

    configure rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'shoulda/matchers'
 require_relative 'support/factory_bot'
 require_relative 'support/chrome'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -65,4 +66,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end

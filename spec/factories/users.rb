c2d2a540af68e9c1f30209c923142801f40ac94b
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-07-30 22:59:11 -0400

    update factory bot file

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,23 @@
 FactoryBot.define do
-  factory(:user) do
+  factory :user do
     email { Faker::Internet.email }
-    password { Faker::Internet.password }
+    password { 'password123' }
+    password_confirmation { 'password123' }
+
+    trait :client do
+      role { :client }
+    end
+
+    trait :therapist do
+      role { :therapist }
+    end
+
+    trait :admin do
+      role { :admin }
+    end
+
+    factory :client, traits: [:client]
+    factory :therapist, traits: [:therapist]
+    factory :admin, traits: [:admin]
   end
 end

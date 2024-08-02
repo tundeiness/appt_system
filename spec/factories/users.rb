bf4f82941ff4a8592a60f4a3604bc4d5b7f54b7b
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-08-02 15:14:29 -0400

    edit factortbot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone_number { Faker::PhoneNumber.phone_number_with_country_code }
     email { Faker::Internet.email }
     password { 'password123' }
     password_confirmation { 'password123' }

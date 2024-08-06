d38c2d658a2bd8bf21717b4245de5f71f3c20d7e
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-08-06 18:12:26 -0400

    test end time after start time

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,16 @@
 class Appointment < ApplicationRecord
-  belongs_to :therapist
-  belongs_to :client
+  belongs_to :therapist, class_name: 'User'
+  belongs_to :client, class_name: 'User', optional: true
+
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validate :end_time_after_start_time
+
+  private
+
+  def end_time_after_start_time
+    return if end_time.blank? || start_time.blank?
+
+    errors.add(:end_time, 'must be after start time') if end_time <= start_time
+  end
 end
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
   describe 'validations' do
-    # before do
-    #   let(:therapist) { FactoryBot.create(:user, :therapist) }
-    #   let(:client) { FactoryBot.create(:user, :client) }
-    #   let(:admin) { FactoryBot.create(:user, :admin) }
-    # end
-
     let(:therapist) { FactoryBot.create(:user, :therapist) }
     let(:client) { FactoryBot.create(:user, :client) }
     let(:admin) { FactoryBot.create(:user, :admin) }
@@ -20,6 +14,39 @@ RSpec.describe Appointment, type: :model do
         end_time: DateTime.now + 1.hour
       )
       expect(appointment).to be_valid
+    end
+
+    it 'is not valid without a start_time' do
+      appointment = Appointment.new(
+        therapist:,
+        client:,
+        start_time: nil,
+        end_time: DateTime.now + 1.hour
+      )
+      appointment.valid?
+      expect(appointment.errors[:start_time]).to include("can't be blank")
+    end
+
+    it 'is not valid without an end_time' do
+      appointment = Appointment.new(
+        therapist:,
+        client:,
+        start_time: DateTime.now,
+        end_time: nil
+      )
+      appointment.valid?
+      expect(appointment.errors[:end_time]).to include("can't be blank")
+    end
+
+    it 'is not valid if end_time is before start_time' do
+      appointment = Appointment.new(
+        therapist:,
+        client:,
+        start_time: DateTime.now,
+        end_time: DateTime.now - 1.hour
+      )
+      appointment.valid?
+      expect(appointment.errors[:end_time]).to include('must be after start time')
     end
   end
 end

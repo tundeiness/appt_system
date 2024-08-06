d0dd125abb1cce37225aa2255dc774ec4ba8b07c
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-08-06 16:18:39 -0400

    implement auth & authorization feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  load_and_authorize_resource unless: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
 end

8257e011af6c868d420d7f15e67f2f9403f325bc
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-08-08 23:31:11 -0400

    add more method and scope

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,24 @@
 class Schedule < ApplicationRecord
   belongs_to :therapist, class_name: 'User'
+
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validate :end_time_after_start_time
+  validate :therapist_or_admin
+
+  scope :available, -> { where('end_time > ?', Time.current) }
+
+  private
+
+  def end_time_after_start_time
+    return if end_time.blank? || start_time.blank?
+
+    errors.add(:end_time, 'must be after start time') if end_time <= start_time
+  end
+
+  def therapist_or_admin
+    return if therapist&.therapist? || therapist&.admin?
+
+    errors.add(:therapist, 'must have therapist or admin role')
+  end
 end

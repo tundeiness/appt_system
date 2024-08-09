3c753b9d312def20a283c8aec5b859963377ed17
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-08-08 22:36:59 -0400

    edit change method

--- db/migrate/20240809023347_create_availabilities.rb
+++ db/migrate/20240809023347_create_availabilities.rb
@@ -1,11 +1,14 @@
 class CreateAvailabilities < ActiveRecord::Migration[7.1]
   def change
     create_table :availabilities do |t|
-      t.references :therapist, null: false, foreign_key: true
-      t.datetime :start_time
-      t.datetime :end_time
+      t.references :therapist, null: false, foreign_key: { to_table: :users }
+      t.datetime :start_time, null: false
+      t.datetime :end_time, null: false
 
       t.timestamps
     end
+
+    # Add an index to ensure that there are no overlapping availabilities for a therapist
+    add_index :availabilities, %i[therapist_id start_time end_time], unique: true
   end
 end

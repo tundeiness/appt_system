da2c44ad373fb884fb1d324ef7a28c22b1492213
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-08-11 20:19:14 -0400

    edit spec file

--- spec/models/pair_spec.rb
+++ spec/models/pair_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Pair, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:client) { FactoryBot.create(:user, role: :client) }
+    let(:therapist) { FactoryBot.create(:user, role: :therapist, specialization: 'Cognitive Behavioral Therapy') }
+
+    it 'is valid with valid attributes' do
+      pair = FactoryBot.build(:pair, client:, therapist:)
+      expect(pair).to be_valid
+    end
+  end
 end

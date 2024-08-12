e7f1a920aaafae88a2ca8503aed67ed5581c81cc
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-08-11 21:16:23 -0400

    clean up

--- spec/models/pair_spec.rb
+++ spec/models/pair_spec.rb
@@ -6,23 +6,23 @@ RSpec.describe Pair, type: :model do
     let(:therapist) { FactoryBot.create(:user, role: :therapist, specialization: 'Cognitive Behavioral Therapy') }
 
     it 'is valid with valid attributes' do
-      pair = FactoryBot.build(:pair, client:, therapist:)
+      pair = build(:pair, client:, therapist:)
       expect(pair).to be_valid
     end
 
     it 'is invalid without a client' do
-      pair = FactoryBot.build(:pair, client: nil, therapist:)
+      pair = build(:pair, client: nil, therapist:)
       expect(pair).to_not be_valid
     end
 
     it 'is invalid without a therapist' do
-      pair = FactoryBot.build(:pair, client:, therapist: nil)
+      pair = build(:pair, client:, therapist: nil)
       expect(pair).to_not be_valid
     end
 
     it 'is invalid if the pair already exists' do
-      FactoryBot.create(:pair, client:, therapist:)
-      duplicate_pair = FactoryBot.build(:pair, client:, therapist:)
+      create(:pair, client:, therapist:)
+      duplicate_pair = build(:pair, client:, therapist:)
       expect(duplicate_pair).to_not be_valid
     end
   end
